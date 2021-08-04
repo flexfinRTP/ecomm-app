0f7f171e44898c76667421178ae1f48586e36fcc
flexfinRTP <gaffney311@outlook.com>
2021-08-04 01:16:02 -0500

    product routes, error handling

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const dontenv = require('dotenv');
+const { notFound, errorHandler }= require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
-const products = require('./data/products');
+
+const productRoutes = require('./routes/productsRoutes');
 
 dontenv.config();
 
@@ -13,13 +15,19 @@ app.get('/', (req, res) => {
     res.send('API is running')
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+app.use('api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product);
-});
+app.use(notFound);
+
+app.use(errorHandler);
+
+// app.get('/api/products', (req, res) => {
+//     res.json(products);
+// });
+
+// app.get('/api/products/:id', (req, res) => {
+//     const product = products.find(p => p._id === req.params.id)
+//     res.json(product);
+// });
 
 app.listen(5000, console.log('Server running on port 5000'));
