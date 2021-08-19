899617e94a64e9273264cb4e35cc01e91cdb47aa
flexfinRTP <gaffney311@outlook.com>
2021-08-19 00:01:44 -0500

    logout action

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { LinkContainer } from 'react-router-bootstrap';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-
-
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import { logout } from '../actions/userActions';
 
 const Header = () => {
+    const dispatch = useDispatch()
+  
+    const userLogin = useSelector((state) => state.userLogin)
+    const { userInfo } = userLogin
+  
+    const logoutHandler = () => {
+      dispatch(logout())
+    }
+
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -12,7 +21,7 @@ const Header = () => {
                     <LinkContainer to='/'>
                         <Navbar.Brand>Missouri River Valley Clothing Co.</Navbar.Brand>
                     </LinkContainer>
-                    
+
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
                     <Navbar.Collapse id="basic-navbar-nav">
@@ -20,9 +29,23 @@ const Header = () => {
                             <LinkContainer to='/cart'>
                                 <Nav.Link><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
                             </LinkContainer>
-                            <LinkContainer to='/login'>
-                                <Nav.Link><i className="fas fa-user"></i>Sign In</Nav.Link>
-                            </LinkContainer>
+
+                            {userInfo ? (
+                                <NavDropdown title={userInfo.name} id='username'>
+                                    <LinkContainer to='/profile'>
+                                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <NavDropdown.Item onClick={logoutHandler}>
+                                        Logout
+                                    </NavDropdown.Item>
+                                </NavDropdown>
+                            ) : (
+                                <LinkContainer to='/login'>
+                                    <Nav.Link>
+                                        <i className='fas fa-user'></i> Sign In
+                                    </Nav.Link>
+                                </LinkContainer>
+                            )}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
@@ -31,4 +54,4 @@ const Header = () => {
     )
 }
 
-export default Header
+export default Header;
