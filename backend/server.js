9507e372304bfe77138adc1673bed9c406d54fcd
flexfinRTP <gaffney311@outlook.com>
2021-08-20 00:55:28 -0500

    create order route

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const connectDB = require('./config/db');
 
 const productRoutes = require('./routes/productsRoutes');
 const userRoutes = require('./routes/userRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+
 
 dontenv.config();
 
@@ -20,6 +22,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 
