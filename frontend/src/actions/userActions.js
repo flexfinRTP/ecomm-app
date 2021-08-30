1a927e530b816c385de5ff710ae7328ddda6a32b
flexfinRTP <gaffney311@outlook.com>
2021-08-29 20:34:20 -0500

    user orders arc && admin route

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -7,7 +7,10 @@ import { USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_LOG
     USER_REGISTER_SUCCESS, 
     USER_UPDATE_PROFILE_FAIL, 
     USER_UPDATE_PROFILE_REQUEST,
-    USER_UPDATE_PROFILE_SUCCESS} from "../constants/userConstants"
+    USER_UPDATE_PROFILE_SUCCESS,
+    USER_DETAILS_RESET,
+} from "../constants/userConstants";
+import { ORDER_LIST_MY_RESET } from "../constants/orderConstants";
 import axios from 'axios';
 
 export const login = (email, password) => async (dispatch) => {
@@ -41,6 +44,8 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
     localStorage.removeItem('userInfo');
     dispatch({ type: USER_LOGOUT });
+    dispatch({ type: USER_DETAILS_RESET });
+    dispatch({ type: ORDER_LIST_MY_RESET });
 }
 
 export const register = (name, email, password) => async (dispatch) => {
--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import { Form, Button, Col } from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux';
-import FormContainer from '../components/FormContainer';
-import CheckoutSteps from '../components/CheckoutSteps';
-import { savePaymentMethod } from '../actions/cartActions';
+import React, { useState } from 'react'
+import { Form, Button, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import FormContainer from '../components/FormContainer'
+import CheckoutSteps from '../components/CheckoutSteps'
+import { savePaymentMethod } from '../actions/cartActions'
 
 const PaymentScreen = ({ history }) => {
     const cart = useSelector((state) => state.cart)
     const { shippingAddress } = cart
 
-    if (!shippingAddress) {
-        history.push('/shipping');
+    if (!shippingAddress.address) {
+        history.push('/shipping')
     }
 
     const [paymentMethod, setPaymentMethod] = useState('PayPal')
@@ -30,24 +30,24 @@ const PaymentScreen = ({ history }) => {
             <Form onSubmit={submitHandler}>
                 <Form.Group>
                     <Form.Label as='legend'>Select Method</Form.Label>
-
                     <Col>
                         <Form.Check
                             type='radio'
                             label='PayPal or Credit Card'
-                            id='PayPal' name='paymentMethod'
+                            id='PayPal'
+                            name='paymentMethod'
                             value='PayPal'
                             checked
-                            onChange={(e) => setPaymentMethod(e.target.value)}>
-                        </Form.Check>
-                        {/* <Form.Check 
-                            type='radio'
-                            label='Stripe'
-                            id='Stripe'
-                            name='paymentMethod'
-                            value='Stripe'
-                            onChange={(e) => setPaymentMethod(e.target.value)}>
-                            </Form.Check> */}
+                            onChange={(e) => setPaymentMethod(e.target.value)}
+                        ></Form.Check>
+                        {/* <Form.Check
+              type='radio'
+              label='Stripe'
+              id='Stripe'
+              name='paymentMethod'
+              value='Stripe'
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check> */}
                     </Col>
                 </Form.Group>
 
@@ -59,4 +59,4 @@ const PaymentScreen = ({ history }) => {
     )
 }
 
-export default PaymentScreen;
+export default PaymentScreen
