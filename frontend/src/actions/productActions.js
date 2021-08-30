b2ab86d42a057874c91e32f4a5159cfcd6080f8f
flexfinRTP <gaffney311@outlook.com>
2021-08-30 00:58:12 -0500

    admin productlist delete

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,15 +1,18 @@
 import axios from 'axios';
-import { 
+import {
     PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
     PRODUCT_LIST_FAIL,
     PRODUCT_DETAILS_REQUEST,
     PRODUCT_DETAILS_SUCCESS,
     PRODUCT_DETAILS_FAIL,
- } from '../constants/productConstants';
+    PRODUCT_DELETE_REQUEST,
+    PRODUCT_DELETE_SUCCESS,
+    PRODUCT_DELETE_FAIL,
+} from '../constants/productConstants';
 
 
- export const listProducts = () => async (dispatch) => {
+export const listProducts = () => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_LIST_REQUEST })
 
@@ -25,9 +28,9 @@ import {
             payload: error.response && error.response.data.message ? error.response.data.message : error.message
         })
     }
- }
+}
 
- export const listProductDetails = (id) => async (dispatch) => {
+export const listProductDetails = (id) => async (dispatch) => {
     try {
         dispatch({ type: PRODUCT_DETAILS_REQUEST })
 
@@ -43,4 +46,32 @@ import {
             payload: error.response && error.response.data.message ? error.response.data.message : error.message
         })
     }
- }
+}
+
+export const deleteProduct = (id) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: PRODUCT_DELETE_REQUEST
+        })
+
+        const { userLogin: { userInfo } } = getState();
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+
+        await axios.delete(`/api/products/${id}`, config)
+
+        dispatch({
+            type: PRODUCT_DELETE_SUCCESS
+        })
+
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_DELETE_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
+    }
+}
