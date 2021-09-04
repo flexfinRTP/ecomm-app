4801549187e3f5e68560b2239c2cbe3a42396d12
flexfinRTP <gaffney311@outlook.com>
2021-09-04 14:59:16 -0500

    carousel arc, screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,6 +6,7 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 import Paginate from '../components/Paginate'
 import { listProducts } from '../actions/productActions'
+import ProductCarousel from '../components/ProductCarousel'
 
 const HomeScreen = ({ match }) => {
     const keyword = match.params.keyword
@@ -13,14 +14,17 @@ const HomeScreen = ({ match }) => {
     const pageNumber = match.params.pageNumber || 1
 
     const dispatch = useDispatch()
+
     const productList = useSelector((state) => state.productList)
     const { loading, error, products, page, pages } = productList
 
     useEffect(() => {
         dispatch(listProducts(keyword, pageNumber))
     }, [dispatch, keyword, pageNumber])
+
     return (
         <>
+        {!keyword && <ProductCarousel />}
             <h1>Latest Products</h1>
             {loading ? (
                 <Loader />
