a5fc8c96cee0ed21411f11da3139169155756580
flexfinRTP <gaffney311@outlook.com>
2022-02-07 00:25:44 -0600

    deploy, carousel fix

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -17,7 +17,7 @@ const ProductCarousel = () => {
     }, [dispatch])
 
     return loading ? (<Loader />) : error ? (<Message variant='danger'>{error}</Message>) : (
-        <Carousel pause='hover' className='bg-warning'>
+        <Carousel pause='hover' className='bg-transparent'>
             {products.map(product => (
                 <Carousel.Item key={product._id}>
                     <Link to={`/product/${product._id}`}>
---
+++ readme.md
@@ -0,0 +1,10 @@
+Ecomm App
+
+How to run:
+
+npm run dev - run both env
+cd frontend/
+npm run start - starts frontend
+
+MongoDB hosted db, MongoDB Atlas/Compass
+Cluster gets paused after inactivity. Check there for backend errors.
